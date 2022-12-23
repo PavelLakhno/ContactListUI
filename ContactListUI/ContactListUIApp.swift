3e2159e0889e4155098e36c78c39530c8badcd37
Pavel <lmr_16@mail.ru>
2022-12-23 11:06:52 +0300

    Initial Commit

---
+++ ContactListUI/ContactListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListUIApp.swift
+//  ContactListUI
+//
+//  Created by user on 23.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
